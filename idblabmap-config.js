aefec24f1b5f7fa288e54d7c2230b68ae38cd175
Julien Collaer <julien.collaer@opengeode.be>
2021-01-12 14:08:27 -0500

    Update idblabmap-config.js
    typo

--- idblabmap-config.js
+++ idblabmap-config.js
@@ -4,10 +4,10 @@ var YEARO = '2019';
 var SMALLYEARO = '19';
 
 var OPERATIONS_GEOJSON = (window.location.href.indexOf("file:")==-1 || true ?
-//"https://raw.githubusercontent.com/collaer/idblabmap/master/DATA/operations-snapshot-2020.geojson"
-"./DATA/operations-anonymized-2019.geojson"
+"https://raw.githubusercontent.com/collaer/idblabanonymap/master/DATA/operations-anonymized-2019.geojson"
 :
-"./DATA/operations-anonymized-2019.geojson");
+"https://raw.githubusercontent.com/collaer/idblabanonymap/master/DATA/operations-anonymized-2019.geojson"
+//"./DATA/operations-anonymized-2019.geojson");
 
 var COUTRIES_GEOJSON = (window.location.href.indexOf("file:")==-1 || true ?
 //"https://raw.githubusercontent.com/collaer/idblabmap/master/DATA/countries2.geojson"
